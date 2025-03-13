258996bf724d9fcac2b40ad2665a49922f258bb0
Narik <narikdaniel2016@gmail.com>
2025-03-13 13:40:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Programa simple en Python que saluda al usuario
+
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Mostrar un saludo personalizado
+print(f"¡Hola, {nombre}! Bienvenido al mundo de Python.")
